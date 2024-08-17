cc8e39c5ded0baf5b7e04218ba299b163a068cd2
Anand Mishra <125855181+anandmi882635@users.noreply.github.com>
2024-08-17 13:11:54 +0000

    variable done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,24 @@
+const accountId = 488348
+let accountEmail = 'anand@gmail.com'
+var accountPassword = '5432'
+accountCity = 'hyderabad'
+
+let accountstate;
+
+// if declare variable and that variable is has no values then it shows you 'undefined'
+
+// accountNumber = 434334434
+accountEmail = 'mishra@gmail.com'
+accountPassword = '343adb'
+accountCity = 'Bengularu'
+// console.log(accountNumber);
+
+/*
+not preffered to use var type variable
+use const and let variable type
+*/
+
+
+// console.log([accountEmail, accountPassword, accountCity])
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,5 @@
 console.log("Anand")
+var a = 333
+var b = 444
+var c = a + b
+console.log (c)
